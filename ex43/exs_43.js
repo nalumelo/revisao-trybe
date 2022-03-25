5670e2f0d2f41c72d02aa8b47ec7c9c2743e4347
Nalu Pantoja <aluziapantoja@gmail.com>
2022-03-25 19:14:37 -0300

    Exs 2 e 3

--- ex43/exs_43.js
+++ ex43/exs_43.js
@@ -1,8 +1,34 @@
 //ex 1
-let fatorial = 0;
-for (let index = 10; index > 0; index -= 1) {
-    let ant = index - 1;
-    fatorial 
+let fatorial = 10;
+for (let index = 9; index > 0; index -= 1) {
+    //multiplicar atual por atual - 1
+    //salvar atual em uma variavel
+    fatorial = fatorial * index;
 };
 
 console.log(fatorial);
+
+//ex 2
+let word = 'tryber';
+let array = word.split("");
+let texto = '';
+
+for (let indexx = (array.length - 1); indexx >= 0; indexx -= 1) {
+    let letra = array[indexx];
+    texto = texto + letra;
+};
+
+console.log(texto);
+
+//ex 3
+let arrayy = ['java', 'javascript', 'python', 'html', 'css'];
+let bigger = '';
+for (let index = 0; index < arrayy.length; index += 1) {
+if ((arrayy[index]).length > bigger.length) {
+        bigger = arrayy[index];
+    } else {
+        bigger = bigger;
+    }
+};
+
+console.log(bigger);
