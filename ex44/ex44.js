f5e75432bfb8b4dc1822d2441c0de1008a80c5d5
Nalu Pantoja <aluziapantoja@gmail.com>
2022-03-29 18:32:19 -0300

    Ex 6

--- ex44/ex44.js
+++ ex44/ex44.js
@@ -36,3 +36,21 @@ for (let key in info) {
         console.log(info[key] + ' e ' + info2[key]);
     }
 };
+
+
+
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+};
+
+//ex 6
+console.log(`O filme favorito de ${leitor.nome} ${leitor.sobrenome} se chama '${leitor.livrosFavoritos[0].titulo}'`);
