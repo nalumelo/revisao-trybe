13bcdb21f00cb07288c543a7ea51c6612c561065
Nalu Pantoja <aluziapantoja@gmail.com>
2022-06-01 17:39:35 -0300

    Adiciona h1 por meio de js

---
+++ ex52/exs/script.js
@@ -0,0 +1,6 @@
+//ex 1
+let body = document.querySelector('body');
+let titulo = 'Exercício 5.2 - JavaScript DOM';
+let h1 = document.createElement('h1');
+h1.innerText = titulo;
+body.appendChild(h1);
