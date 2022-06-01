348dc647e2ec51252957a75f34e77436dca8f5d1
Nalu Pantoja <aluziapantoja@gmail.com>
2022-06-01 17:13:06 -0300

    Ex 2 pt 2

--- ex52/pt2/script.js
+++ ex52/pt2/script.js
@@ -1,7 +1,15 @@
 //ex 1
 let paiDoPai = document.getElementById('paiDoPai');
-let texto = 'Texto 1';
+let texto1 = 'Texto 1';
 let teste1 = document.createElement('p');
-teste1.innerText = texto;
+teste1.innerText = texto1;
 
 paiDoPai.appendChild(teste1);
+
+//ex 2
+let elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
+let texto2 = 'Texto 2';
+let teste2 = document.createElement('p');
+teste2.innerText = texto2;
+
+elementoOndeVoceEsta.appendChild(teste2);
