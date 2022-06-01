fe78b42280c468a3d3cc03a2c3fe74de1827d96a
Nalu Pantoja <aluziapantoja@gmail.com>
2022-06-01 17:41:54 -0300

    Adiciona tag main com classe main-content

--- ex52/exs/script.js
+++ ex52/exs/script.js
@@ -4,3 +4,8 @@ let titulo = 'Exercício 5.2 - JavaScript DOM';
 let h1 = document.createElement('h1');
 h1.innerText = titulo;
 body.appendChild(h1);
+
+//ex 2
+let main = document.createElement('main');
+main.className = 'main-content';
+body.appendChild(main);
